fafdce08fa94ef19b2d5970ecafa692c30d20d39
Jean Piero <jeanpp20@gmail.com>
2026-05-17 15:46:40 -0500

    fix cors for vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://hexagon-design-landing-page-project.vercel.app/"
+  ],
   credentials: true,
 }));
 
