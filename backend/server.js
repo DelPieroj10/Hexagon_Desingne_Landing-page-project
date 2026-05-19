e5a91d4d4d3768d1fa448725fc68c80389346de5
Jean Piero <jeanpp20@gmail.com>
2026-05-18 19:52:42 -0500

    fix app.listen in server component

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ app.get("/test", (req, res) => {
   res.send("test ok");
 });
 
-app.listen(onRender, () => {
-  console.log("Server running on port onRender 🚀");
+app.listen(3001, () => {
+  console.log("Server running on port 3001 🚀");
 });
